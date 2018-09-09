3a9a42b890032c835467ad1995c3b1297fdb8910
Dana Terry <Dana@Danas-MacBook-Air.local>
2018-09-09 12:29:39 -0400

    added home page, css, updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  # root to: 'visitors#index'
+  root to: 'static#home'
+  get '/' => 'static#home'
+  get 'tech' => 'static#tech'
+  get 'art' => 'static#art'
 end
